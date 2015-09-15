6690972ef5e9d3a0a2d41679b1e8fd14145484fd
Chris Hoffman <chris.c.hoffman@gmail.com>
2015-09-15 09:14:50 -0400

    change package / module names to fit intent

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_dnb_pomodoro_session'
+Rails.application.config.session_store :cookie_store, key: 'react_hot_reload_rails'
+
